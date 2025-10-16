cc147a8bf2f1c9a494a1d365f706e70a235aa2b8
alyvnihad <aliyevnihad011@gmail.com>
2025-10-16 16:39:11 +0400

    ATM model class

---
+++ src/main/java/org/example/atmservice/model/ATM.java
@@ -0,0 +1,15 @@
+package org.example.atmservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class ATM {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
